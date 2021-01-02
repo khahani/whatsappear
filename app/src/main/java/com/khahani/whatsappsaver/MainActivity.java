8b1f66aa2b950a3a3dcece012f3b15c247386d21
Mohammadreza khahani <mr.khahani@gmail.com>
2021-01-02 19:58:27 +0330

    config

--- app/src/main/java/com/khahani/whatsappsaver/MainActivity.java
+++ app/src/main/java/com/khahani/whatsappsaver/MainActivity.java
@@ -1,7 +1,7 @@
-package com.example.myfirstapp;
+package com.khahani.whatsappsaver;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
